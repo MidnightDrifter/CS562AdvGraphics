d2401f88cdcfdc6e0c59ed26646fdb7a0a33bd06
MidnightDrifter <sean.higgins@digipen.edu>
2017-11-14 15:29:12 -0800

    Spherical harmonics hard part done, just gotta A. write it to an HDR texture or B. throw it to the shader and calculate from there

--- CS541-framework/texture.h
+++ CS541-framework/texture.h
@@ -3,18 +3,24 @@
 // to read an image file into a texture, and methods to bind a texture
 // to a shader for use, and unbind when done.
 ////////////////////////////////////////////////////////////////////////
-
+#include <vector>
 #ifndef _TEXTURE_
 #define _TEXTURE_
+
+
+struct basicVec3
+{
+	float x, y, z;
+};
 
 class Texture
 {
  public:
     unsigned int textureId;
 	int HDRwidth, HDRheight;
-		
+	std::vector<basicVec3> HDRvals;
 	//Sean added this
-	Texture() : textureId(0), HDRwidth(-1), HDRheight(-1) {}
+	Texture() : textureId(0), HDRwidth(-1), HDRheight(-1), HDRvals() {}
 	Texture(int width, int height);
 
 
